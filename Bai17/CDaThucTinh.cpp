be87b28454976d2b330bf40fe73d797459eb44bc
TuTTC <146366273+TuTTC@users.noreply.github.com>
2024-05-12 03:15:17 +0700

    Update CDaThucTinh.cpp

--- Bai17/CDaThucTinh.cpp
+++ Bai17/CDaThucTinh.cpp
@@ -15,10 +15,62 @@ istream& operator>>(istream& is, CDaThucTinh& P)
 
 ostream& operator<<(ostream& out, CDaThucTinh P)
 {
-    int check = 0;
-    for (int i = P.n - 1; i >= 0; i--) {
-    }
+   /* int check = 0;
+ for (int i = 0; i < P.n; i++)
+ {
+     for (int j = 0; j < 2; j++)
+     {
+         if (j == 0)
+         {
+             if (P.a[i][j] == 0) {
+                 continue;
+             }
+             else if ((P.a[i][j] == 1)) {
+                 out << (check ? "+" : "");
+                 check = 1;
+                 if (!(P.a[i][j + 1])) {
+                     out << 1;
+                     continue;
+                 }
+             }
+             else if ((P.a[i][j] == -1))
+             {
+                 out << "-";
+                 check = 1;
+                 if (!(P.a[i][j + 1])) {
+                     out << 1;
+                     continue;
+                 }
+             }
+             else
+             {
+                 out << (((P.a[i][j] > 0) && check) ? "+" : "") << P.a[i][j];
+                 check = 1;
+             }
+         }
+         if (j == 1)
+         {
+             if (P.a[i][j] == 0)
+             {
+                 continue;
+             }
+             else if ((P.a[i][j] == 1)) {
+                 check = 1;
+                 out << "x";
+             }
+             else
+             {
+                 check = 1;
+                 out << "x^" << P.a[i][j];
+             }
+         }
 
-    return out;
+     }
+    
+ } 
+ if (!check)
+         out << 0;
+
+ return out;*/
 }
 
